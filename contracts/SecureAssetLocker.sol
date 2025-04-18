80be4b50251b8a0de497be9ab8617036e037e3f0
laurence <laurence@extropy.io>
2025-04-18 10:25:35 +0000

    Initial passong test

--- contracts/SecureAssetLocker.sol
+++ contracts/SecureAssetLocker.sol
@@ -2,8 +2,27 @@
 pragma solidity ^0.8.4;
 
 import "@lukso/LSP4DigitalAssetMetadata/LSP4DigitalAssetMetadata.sol";
+import "@lukso/LSP6KeyManager/LSP6KeyManager.sol";
+
 
 
 contract SecureAssetLocker is LSP4DigitalAssetMetadata {
+
+    address public universalProfile;
+    LSP6KeyManager keyManager;
+
+constructor(
+        address _universalProfile,
+        address _keyManager,
+        string memory _tokenName,
+        string memory _tokenSymbol,
+        address _newOwner
+    ) 
+        LSP4DigitalAssetMetadata(_tokenName, _tokenSymbol, _newOwner) 
+    {
+        universalProfile = _universalProfile;
+        keyManager = LSP6KeyManager(_keyManager);
+    }
+
 
 }
--- test/SecureAssetLocker.t.sol
+++ test/SecureAssetLocker.t.sol
@@ -8,18 +8,32 @@ import "../contracts/mocks/MockLSP7.sol";
 contract SecureAssetLockerTest is Test {
     SecureAssetLocker locker;
     MockLSP7 token;
-   
-    function setUp() public {
+    address owner = address(0xABCD);
+    address otherUser = address(0xBEEF);
+
+    bytes32 LOCK_ASSET_PERMISSION = keccak256("LOCK_ASSET");
+    bytes32 UNLOCK_ASSET_PERMISSION = keccak256("UNLOCK_ASSET");
+
+    bytes32 assetId = keccak256("asset-1");
+
+
+function setUp() public {
         // Deploy a mock LSP7 token
         token = new MockLSP7();
 
         // Mint tokens to owner
         token.mint(owner, 1000 ether);
 
-        // Deploy the SecureAssetLocker (with mock UP and KeyManager addresses)
-        locker = new SecureAssetLocker(address(1), address(2));
+        // Deploy the SecureAssetLocker with correct parameters
+        locker = new SecureAssetLocker(
+            address(1),                    // universalProfile (mocked)
+            address(2),                    // keyManager (mocked)
+            "Secure Asset Locker",         // tokenName
+            "SAL",                         // tokenSymbol
+            owner                          // newOwner (Universal Profile owner address)
+        );
 
-  
+
     }
 
     function testLockAssetSuccess() public {
